ff04374b0b241b8403fd522b89bbedd0ac748a69
KristiBu <106319724+KristiBu888@users.noreply.github.com>
2023-10-15 15:55:57 +0300

    task_1 ready2

--- task_1.py
+++ task_1.py
@@ -2,11 +2,11 @@
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0)
 
-num = int(input("Введите трехзначное число: "))
+n = int(input("Введите трехзначное число: "))
 
-digit1 = num // 100
-digit2 = (num % 100) // 10
-digit3 = num % 10
+digit1 = n // 100
+digit2 = (n % 100) // 10
+digit3 = n % 10
 
 sum_of_digits = digit1 + digit2 + digit3
 print(f"{digit1} + {digit2} + {digit3} = {sum_of_digits}")
